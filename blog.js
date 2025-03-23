a4a2fea89b5fe9686f9b78d94109515b1f92e101
ravidassnz <ravidassnz@gmail.com>
2025-03-24 10:34:25 +1300

    remade blog with responsive pages and added subpages

---
+++ blog.js
@@ -0,0 +1,17 @@
+// function myFunction() {
+//   let x = document.getElementById('myMainNav')
+//   if (x.className === 'mainNav') {
+//     x.className += ' responsive'
+//   } else {
+//     x.className = 'mainNav'
+//   }
+// }
+
+function myFunction() {
+  var x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'topnav'
+  }
+}
